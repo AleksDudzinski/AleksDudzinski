dbbf48fc678b1518e3648d27b2a10a105b17053e
AleksDudzinski <lucek.dudzinski@gmail.com>
2025-11-10 18:51:06 +0100

    Pong

---
+++ Assets/Ball.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        Launch();
+    }
+
+    public void Reset()
+    {
+        rb.linearVelocity = Vector2.zero;
+        transform.position = startPosition;
+        Launch();
+    }
+    void Update()
+    {
+
+    }
+
+    private void Launch()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;
+        rb.linearVelocity = new Vector2(speed * x, speed * y);
+    }
+}
